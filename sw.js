7a94ba4de9ca5920f2aae44ec602eb3d0ab20e06
TOJI <mos22751@gmail.com>
2026-09-24 02:56:48 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'ga8-pwa-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+// إشعار وصول التنبيه المنبثق حتى لو التطبيق مقفول
+self.addEventListener('push', event => {
+  const options = {
+    body: event.data ? event.data.text() : 'تبدأ إحدى محاضراتك خلال 15 دقيقة',
+    icon: './icon.png',
+    badge: './icon.png',
+    vibrate: [200, 100, 200, 100, 200],
+  };
+  event.waitUntil(self.registration.showNotification('تنبيه محاضرة GA8 ⏰', options));
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('./index.html'));
+});
