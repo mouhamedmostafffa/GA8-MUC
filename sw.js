1e369489e515069ffefe05d5fceb6a9d7981fb9f
TOJI <mos22751@gmail.com>
2026-09-24 10:25:10 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'ga8-pwa-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.png'];
+const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './bg.jpg'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
