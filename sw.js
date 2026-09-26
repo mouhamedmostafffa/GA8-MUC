b18a4678ec4abda0dfafb3b849c703bbe7704153
TOJI <mos22751@gmail.com>
2026-09-26 15:02:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ga8-pwa-v2';
+const CACHE_NAME = 'ga8-pwa-v1';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.png', './bg.jpg'];
 
 self.addEventListener('install', event => {
